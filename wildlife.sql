9c2d8ffc157e4dc05dd697710a1663912d111c76
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2025-06-03 19:05:15 +0600

    created species table

--- wildlife.sql
+++ wildlife.sql
@@ -7,3 +7,15 @@ Create Table rangers(
 )
 
 -- create the table species
+CREATE Table species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE,
+    conservation_status VARCHAR(50) CHECK (
+        conservation_status IN (
+            'Endangered',
+            'Vulnerable'
+        )
+    )
+);
