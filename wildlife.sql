c179ba6e1354b42147e85c39c8104fe077e00b0a
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2025-06-04 02:03:20 +0600

    added data into sightings table

--- wildlife.sql
+++ wildlife.sql
@@ -28,3 +28,29 @@ CREATE TABLE sightings(
     sighting_time TIMESTAMP NOT NULL,
     notes VARCHAR(200)
 );
+
+CREATE TABLE sightings(
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT NOT NULL REFERENCES rangers(ranger_id) ON DELETE CASCADE,
+    species_id INT NOT NULL  REFERENCES species(species_id) ON DELETE CASCADE,
+    "location" VARCHAR(50),
+    sighting_time TIMESTAMP NOT NULL,
+    notes VARCHAR(200)
+);
+-- insert data into rangers table 
+
+INSERT INTO  rangers(name, region) 
+    VALUES('Alice Green', 'Northern Hills'),
+          ('Bob White', 'River Delta'),
+          ('Carol King', 'Mountain Range');
+
+-- insert data into species table
+
+INSERT INTO species(common_name, scientific_name, discovery_date, conservation_status) VALUES ('Snow Leopard', 'Pantera uncia', '1775-01-01', 'Endangered'),('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'), ('Red Panda', 'Ailurus flgens', '1825-01-01', 'Vulnerable'),('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+--isert data into sightings table
+INSERT INTO sightings(species_id, ranger_id, "location", sighting_time, notes ) VALUES(1,1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera tram image c'), (2,2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'), (3,3,'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'), (1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+SELECT * from sightings;
+
