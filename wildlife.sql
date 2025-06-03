4b6dd5a6de513d346cc3d7f452b6d664e94baf2a
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2025-06-03 18:52:37 +0600

    first commit

---
+++ wildlife.sql
@@ -0,0 +1,9 @@
+-- Active: 1748637167568@@127.0.0.1@5432@wildlife conservation
+Create Table rangers(
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    region VARCHAR(50)
+
+)
+
+-- create the table species
